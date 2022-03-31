146b8a55787886fee93a058def4a19669cdad2a6
charlotteForever <fernweh1001@163.com>
2022-03-31 13:49:46 +0800

    "使用nprogress进度条“

--- src/api/request.js
+++ src/api/request.js
@@ -1,24 +1,28 @@
 // 对axios进行二次封装
 import axios from 'axios'
-
+// 引入nprogress进度条和进度条样式
+import nprogress from 'nprogress'
+import "nprogress/nprogress.css"
 // 创建一个axios实例（？
 const requests = axios.create({
-    // 对requests进行配置
-
     // 设置发送请求的baseURL
     baseURL: '/api',
     // 隔了5s还没收到响应就认为请求超时
-    timeout: 5000
+    timeout: 5000,
 })
 
 // 请求拦截器
 requests.interceptors.request.use((config) => {
+    // 发送请求时启动
+    nprogress.start()
     // config是一个配置对象,该对象里有请求头headers
     return config
 })
 
 // 响应拦截器
 requests.interceptors.response.use((res) => {
+    // 收到响应时终止
+    nprogress.done()
     // 响应成功的回调函数
     return res.data
 }, (error) => {
